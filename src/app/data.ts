22bc26100798e232a066fc9c925cbdbc9867d11e
Antonis Gkoutzamanis <mythoc@gmail.com>
2020-09-22 20:50:10 +0300

    [TODO] Chart.js testing to display data

---
+++ src/app/data.ts
@@ -0,0 +1,5 @@
+export class Data {
+  glucose_level: number;
+  carb_intake: number;
+  measurement_created_date: Date; 
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { User } from 'src/user';
+import { Data, Router } from '@angular/router';
+import { User } from '../user-account/_models/user';
 import { UserService } from '../user-account/user.service';
+
+import { Chart } from 'chart.js';
+// import { multi } from '../data';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +14,41 @@ import { UserService } from '../user-account/user.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public userService: UserService, private router: Router) {
-   
+  // url = 'http://localhost:58617/API/Charts/GetCharts';
+  // data: Data[];
+  // Player = [];
+  // Run = [];
+  // Linechart = [];
+
+  // Start of chart test
+  // multi: any[];
+  // view: any[] = [700, 300];
+
+  // options
+  // legend: boolean = true;
+  // showLabels: boolean = true;
+  // animations: boolean = true;
+  // xAxis: boolean = true;
+  // yAxis: boolean = true;
+  // showYAxisLabel: boolean = true;
+  // showXAxisLabel: boolean = true;
+  // xAxisLabel: string = 'Year';
+  // yAxisLabel: string = 'Population';
+  // timeline: boolean = true;
+  // colorScheme = {
+  //   domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']  };
+
+
+    url = 'http://localhost:9000/v1/team6/sacchon/measurements';
+    data: Data[];
+    glucose_level = [];
+    carb_intake = [];
+    Linechart = [];
+
+  // End of Chart Test
+
+  constructor(private httpClient: HttpClient, public userService: UserService, private router: Router)  {
+    // Object.assign(this, { multi });
   }
   users: User[];
 
@@ -21,6 +57,49 @@ export class HomeComponent implements OnInit {
       this.users = users;
       console.log(users);
     });
+
+    this.httpClient.get(this.url).subscribe((result: Data[]) => {  
+      result.forEach(x => {  
+        this.glucose_level.push(x.glucose_level);  
+        this.carb_intake.push(x.carb_intake);  
+      });  
+      this  
+      this.Linechart = new Chart('canvas', {  
+        type: 'line',  
+        data: {  
+          labels: this.glucose_level,  
+          datasets: [  
+            {  
+              data: this.carb_intake,  
+              borderColor: '#3cb371',  
+              backgroundColor: "#0000FF",  
+            }  
+          ]  
+        },  
+        options: {  
+          legend: {  
+            display: false  
+          },  
+          scales: {  
+            xAxes: [{  
+              display: true  
+            }],  
+            yAxes: [{  
+              display: true  
+            }],  
+          }  
+        }  
+      });  
+    });  
+  }  
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('account');
+    console.log('user logged out');
+    this.router.navigate(['']);
+  }
+
   }
 
   // get data() {
@@ -34,10 +113,18 @@ export class HomeComponent implements OnInit {
   //   })
   // }
 
-  logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('account');
-    console.log('user logged out');
-    this.router.navigate(['']);
-  }
-}
+  
+
+  //charts start
+  // onSelect(data): void {
+  //   console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  // }
+
+  // onActivate(data): void {
+  //   console.log('Activate', JSON.parse(JSON.stringify(data)));
+  // }
+
+  // onDeactivate(data): void {
+  //   console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  // }
+  //charts end
