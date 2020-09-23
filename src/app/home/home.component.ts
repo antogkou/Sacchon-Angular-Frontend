b18595d832f6ccfa476e7f931d7dcb643ac546a0
Antonis Gkoutzamanis <mythoc@gmail.com>
2020-09-23 13:54:41 +0300

    [TODO] google charts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { UserService } from '../user-account/user.service';
 import { Chart } from 'chart.js';
 // import { multi } from '../data';
 import { HttpClient } from '@angular/common/http';
+import { MeasurementService } from '../patient/measurement.service';
+import { Measurement } from '../patient/measurement';
 
 @Component({
   selector: 'app-home',
@@ -13,44 +15,40 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  url = 'http://localhost:9000/v1/team6/sacchon/measurements';
 
-  // url = 'http://localhost:58617/API/Charts/GetCharts';
-  // data: Data[];
-  // Player = [];
-  // Run = [];
-  // Linechart = [];
+  type = 'LineChart';
+  title = 'Glucose level';
+  data = [
+    ['Firefox', 45.0],
+    ['IE', 26.8],
+    ['Chrome', 12.8],
+    ['Safari', 8.5],
+    ['Opera', 6.2],
+    ['Others', 0.7],
+  ];
+  chartColumns = ['Glucose', 'Carb Intake'];
+  options = {};
+  width = 750;
+  height = 400;
 
-  // Start of chart test
-  // multi: any[];
-  // view: any[] = [700, 300];
-
-  // options
-  // legend: boolean = true;
-  // showLabels: boolean = true;
-  // animations: boolean = true;
-  // xAxis: boolean = true;
-  // yAxis: boolean = true;
-  // showYAxisLabel: boolean = true;
-  // showXAxisLabel: boolean = true;
-  // xAxisLabel: string = 'Year';
-  // yAxisLabel: string = 'Population';
-  // timeline: boolean = true;
-  // colorScheme = {
-  //   domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']  };
-
-
-    url = 'http://localhost:9000/v1/team6/sacchon/measurements';
-    data: Data[];
-    glucose_level = [];
-    carb_intake = [];
-    Linechart = [];
+  glucose_level = [];
+  carb_intake = [];
+  measurement_created_date = [];
+  measurements: Measurement[];
+  dataTable: any;
+  users: User[];
 
   // End of Chart Test
 
-  constructor(private httpClient: HttpClient, public userService: UserService, private router: Router)  {
+  constructor(
+    private httpClient: HttpClient,
+    public userService: UserService,
+    private router: Router,
+    private measurementService: MeasurementService
+  ) {
     // Object.assign(this, { multi });
   }
-  users: User[];
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe((users) => {
@@ -58,40 +56,15 @@ export class HomeComponent implements OnInit {
       console.log(users);
     });
 
-    this.httpClient.get(this.url).subscribe((result: Data[]) => {  
-      result.forEach(x => {  
-        this.glucose_level.push(x.glucose_level);  
-        this.carb_intake.push(x.carb_intake);  
-      });  
-      this  
-      this.Linechart = new Chart('canvas', {  
-        type: 'line',  
-        data: {  
-          labels: this.glucose_level,  
-          datasets: [  
-            {  
-              data: this.carb_intake,  
-              borderColor: '#3cb371',  
-              backgroundColor: "#0000FF",  
-            }  
-          ]  
-        },  
-        options: {  
-          legend: {  
-            display: false  
-          },  
-          scales: {  
-            xAxes: [{  
-              display: true  
-            }],  
-            yAxes: [{  
-              display: true  
-            }],  
-          }  
-        }  
-      });  
-    });  
-  }  
+    this.measurementService.getMeasurements().subscribe((response) => {
+      this.measurements = response;
+      response.map(item => {
+        this.data.push([item.carb_intake, item.measurement_created_date]);
+      })
+      console.log(this.measurements);
+    });
+
+  }
 
   logout() {
     // remove user from local storage to log user out
@@ -99,32 +72,29 @@ export class HomeComponent implements OnInit {
     console.log('user logged out');
     this.router.navigate(['']);
   }
+}
 
-  }
+// get data() {
+//   return this.loginForm.controls;
+// }
 
-  // get data() {
-  //   return this.loginForm.controls;
-  // }  
+// onLogout(){
+//   //debugger
+//   this.userService.login(this.data.email.value, this.data.password.value).subscribe(a => {
+//     this.router.navigate(['/home']);
+//   })
+// }
 
-  // onLogout(){
-  //   //debugger
-  //   this.userService.login(this.data.email.value, this.data.password.value).subscribe(a => {
-  //     this.router.navigate(['/home']);
-  //   })
-  // }
+//charts start
+// onSelect(data): void {
+//   console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+// }
 
-  
+// onActivate(data): void {
+//   console.log('Activate', JSON.parse(JSON.stringify(data)));
+// }
 
-  //charts start
-  // onSelect(data): void {
-  //   console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  // }
-
-  // onActivate(data): void {
-  //   console.log('Activate', JSON.parse(JSON.stringify(data)));
-  // }
-
-  // onDeactivate(data): void {
-  //   console.log('Deactivate', JSON.parse(JSON.stringify(data)));
-  // }
-  //charts end
+// onDeactivate(data): void {
+//   console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+// }
+//charts end
--- src/app/patient/measurement.ts
+++ src/app/patient/measurement.ts
@@ -1,5 +1,5 @@
 export interface Measurement {
     glucose_level: string;
     carb_intake: number;
-    measurement_created_date: Date;
+    measurement_created_date: string;
   }
