6af3faf1983ab4aa189b92b0b984cb03684a180f
Antonis Gkoutzamanis <mythoc@gmail.com>
2020-09-28 16:06:26 +0300

    [ADD] admin show active/inactive doctors

--- src/app/admin/doctor-list/doctor-list.component.ts
+++ src/app/admin/doctor-list/doctor-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { User } from 'src/app/_shared/_models/user';
 import { UserService } from 'src/app/_shared/_services/user.service';
 
@@ -8,11 +9,25 @@ import { UserService } from 'src/app/_shared/_services/user.service';
   styleUrls: ['./doctor-list.component.scss'],
 })
 export class DoctorListComponent implements OnInit {
+  page = 1;
+  pageSize = 5;
   users: User[];
+  inactiveDoctors: any[] = [];
+  inactiveDateForm : FormGroup;
+  // startDate = new FormControl();
+  // endDate = new FormControl();
+  showInactive = false;
+  doctorList = true;
   constructor(public userService: UserService) {}
 
   ngOnInit(): void {
+    this.inactiveDateForm = new FormGroup({
+      startDate: new FormControl(),
+      endDate: new FormControl(),
+    });
+
     this.getAllUsers();
+    this.getInactiveDoctors();
   }
 
   getAllUsers(): void {
@@ -20,5 +35,25 @@ export class DoctorListComponent implements OnInit {
       this.users = response;
       console.log(this.users);
     });
+  }
+
+  getInactiveDoctors(): void {
+    console.log(
+      'start= ' + this.inactiveDateForm.get('startDate').value,
+      'end= ' + this.inactiveDateForm.get('endDate').value
+    );
+    this.userService.getInactiveDoctors(
+      this.inactiveDateForm.get('startDate').value,
+        this.inactiveDateForm.get('endDate').value
+    ).subscribe((response) => {
+      this.inactiveDoctors = response;
+      console.log(this.inactiveDoctors);
+    });
+  }
+
+  showInactiveFunction() {
+    this.showInactive = !this.showInactive;
+    this.doctorList = !this.doctorList;
+  
   }
 }
