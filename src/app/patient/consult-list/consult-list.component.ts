92af5e2e764222e32cbda0ada2ef871f16d012d0
Antonis Gkoutzamanis <mythoc@gmail.com>
2020-10-01 02:09:29 +0300

    [ADD] onDestroy for consult-list

--- src/app/patient/consult-list/consult-list.component.ts
+++ src/app/patient/consult-list/consult-list.component.ts
@@ -7,33 +7,47 @@ import { ConsultService } from '../../_shared/_services/consult.service';
 @Component({
   selector: 'app-consult-list',
   templateUrl: './consult-list.component.html',
-  styleUrls: ['./consult-list.component.scss']
+  styleUrls: ['./consult-list.component.scss'],
 })
 export class ConsultListComponent implements OnInit {
-  constructor(private consultService: ConsultService) { }
+  constructor(private consultService: ConsultService) {}
   page = 1;
   pageSize = 5;
   consults: Consult[];
   private _success = new Subject<string>();
+  private _consultsub$ = new Subject<string>();
   staticAlertClosed = false;
   successMessage = '';
 
-
   ngOnInit(): void {
-    this.consultService.getMyPatientConsults().subscribe((consults) => {
+    this.getMyPatientConsult();
+    this.toastMessage();
+  }
+
+  ngOnDestroy(): void {
+    this._success.unsubscribe();
+    console.log('ngOnDestroy called')
+  }
+
+  getMyPatientConsult() {
+     this.consultService.getMyPatientConsults().subscribe((consults) => {
       this.consults = consults;
       console.log(consults);
     });
+  }
 
-    this._success.subscribe(message => this.successMessage = message);
-    this._success.pipe(
-      debounceTime(5000)
-    ).subscribe(() => this.successMessage = '');
+  toastMessage() {
+    this._success.subscribe((message) => (this.successMessage = message));
+    this._success
+      .pipe(debounceTime(5000))
+      .subscribe(() => (this.successMessage = ''));
 
     this.changeSuccessMessage();
   }
 
   public changeSuccessMessage() {
-    this._success.next(`${new Date().toLocaleDateString()} - Carefully read your latest consultation.`);
+    this._success.next(
+      `${new Date().toLocaleDateString()} - Carefully read your latest consultation.`
+    );
   }
 }
